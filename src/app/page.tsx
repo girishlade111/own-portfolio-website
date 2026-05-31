ec92175de66d2457f27efea8935ad08d1047dc78
Girish Lade <girishlade111@gmail.com>
2026-05-31 03:35:10 -0700

    feat: add Hero component to home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Hero from "@/components/sections/Hero";
+
 export default function Home() {
   return (
     <main className="min-h-screen">
-      {/* Blank dark navy page */}
+      <Hero />
     </main>
   );
 }
